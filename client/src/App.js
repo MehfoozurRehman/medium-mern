30ce87cf4f1760d4db4d09f87262f48bd089794e
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2021-09-12 17:03:46 +0500

    header mostly done

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Styles/styles.scss";
+import Header from "./Components/Header";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
